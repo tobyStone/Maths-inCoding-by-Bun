92cdc590edcb056d28b80f133d9d457c5a94eba0
Toby Stone <40922682+tobyStone@users.noreply.github.com>
2024-01-11 12:28:13 +0000

    Add files via upload

---
+++ test/models/linkedPage.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var layoutSchema = new Schema({
+
+    page = {
+        url_stub: String,
+        description: String,
+        sections: [
+            {
+                id: String,
+                title: String,
+                picture: String,
+                imgAlt: String,
+                link: String,
+            }]
+    },
+});
+
+var layouts = mongoose.model('layouts', layoutSchema)
+
+module.exports = layouts:

db396f873b5dbb4b64fa5d1ec0e0776541f61e7d
Toby Stone <40922682+tobyStone@users.noreply.github.com>
2024-01-11 12:34:39 +0000

    Add files via upload

---
+++ test/controllers/apiController.js
@@ -0,0 +1,29 @@
+var sections = require('./models/linkedPage');
+var bodyParser = require('body-parser');
+const { get } = require('mongoose');
+
+
+module.exports = function(app) {
+
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.get('/', function(req, res) {
+
+        //     CODE IN HERE FOR ITERATING THROUGH PAGES VARIABLE
+        // USING URL STUB AND LINK
+        //TO GET POPULATION OF EACH PAGE
+        //LIKE THIS, BUT MORE... var todos = Todos.findOne().sort('-_id').exec({ todos },
+   //         function(){
+  //                  if(err) throw err;    }
+                    res.render('structure');
+  
+        
+     
+    });
+
+  
+
+        
+}

6b2a16a18cb3992b7054542b5f02e8e6d278b365
Toby Stone <40922682+stickmonster@users.noreply.github.com>
2024-01-24 11:55:46 +0000

    loop in ejs for maths questions file, so as to match questions to selected url... models for maths questions and videos added/changed so schemas represent seeded setupController data

--- test/controllers/apiController.js
+++ test/controllers/apiController.js
@@ -3,26 +3,32 @@ const Layout = require('./models/linkedPage'); // Import your data model
 const Layout = require('./models/videoModel');
 const Layout = require('./models/mathQuestionsModel');
 
+const bodyParser = require('body-parser');
+const Layout = require('./models/Layout'); // Import the Layout model
+const Videos = require('./models/VideoData'); // Import the VideoData model
+const Questions = require('./models/MathQuestions'); // Import the MathQuestions model
+
 module.exports = function (app) {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
 
     app.get('/', async function (req, res) {
         try {
-            const sections = await Layout.find(); // Fetch data from MongoDB
-            const videoData = await Videos.find(); // Fetch data from MongoDB
-            const questionData = await Questions.find(); // Fetch data from MongoDB
+            // Use Promise.all to fetch data from MongoDB simultaneously
+            const [sections, videoData, questionData] = await Promise.all([
+                Layout.find().exec(),
+                Videos.find().exec(),
+                Questions.find().exec(),
+            ]);
 
-            // Render your EJS view and pass the sections as a variable
+            // Render your EJS view and pass the data as variables
             res.render('structureEJS.ejs', { sections });
             res.render('VideoPlayer.ejs', { videoData });
             res.render('maths_questions.ejs', { questionData });
         } catch (err) {
-            console.error('Error fetching sections:', err);
+            console.error('Error fetching data:', err);
             res.status(500).send('Internal Server Error');
         }
     });
 };
-
-
 
--- test/models/videoModel.js
+++ test/models/videoModel.js
@@ -2,27 +2,34 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const sectionSchema = new Schema({
-    id: String,
-    title: String,
-    picture: String,
+const videoSchema = new Schema({
+    video: String,
+    time_stop_1: String,
+    time_stop_2: String,
+    time_stop_3: String,
+    poster: String,
+    imgSrc: String,
     imgAlt: String,
-    link: String,
+    link_questions_1: String,
+    link_questions_2: String,
+    link_questions_3: String,
 });
 
 const pageSchema = new Schema({
     url_stub: String,
     description: String,
-    sections: [sectionSchema], // Embed the section schema as an array
+    videoData: [videoSchema], // Embed the video schema as an array
 });
 
 const layoutSchema = new Schema({
     page: pageSchema,
 });
 
-const Layout = mongoose.model('Layout', layoutSchema, 'layouts'); // Use a singular name for the model
+// Change the export name to 'videoData' instead of 'Layout'
+const videoData = mongoose.model('videoData', layoutSchema, 'maths_through_coding');
 
-module.exports = Layout;
+module.exports = videoData; 
+
 
 
 

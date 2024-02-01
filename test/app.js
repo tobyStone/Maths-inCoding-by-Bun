f05cf1f58947572f579d9d4991fd23278a2cb362
Toby Stone <40922682+stickmonster@users.noreply.github.com>
2024-02-01 17:36:48 +0000

    re-formatting ejs files, restricting length of lines in js files, apiController and setupController both with JSDoc applied, separate JSON files for seeding data and logic changed in setupController, global try/catch in apiController to cut down on repetition

--- test/app.js
+++ test/app.js
@@ -4,46 +4,26 @@ const mongoose = require('mongoose');
 const config = require('./config');
 const setupController = require('./controllers/setupController');
 const apiController = require('./controllers/apiController');
-//const cors = require('cors');
 
-
-
-const port = 3000;
-
-// CORS configuration
-//const corsOptions = {
-//    origin: 'http://localhost:' + port, // or simply true to allow all localhost requests
-//    methods: 'GET' // Adjust methods as needed, GET is common for serving files
-//};
-
+const PORT = 3000;
+const HOST = '0.0.0.0';
 
 app.set('view engine', 'ejs');
 
-//app.use(cors(corsOptions));
-
 app.use('/public', express.static(__dirname + '/public'));
-// Define routes for other pages, using their respective EJS templates.
-
-
-// Serve video files from the Windows filesystem
-//app.use('/videos', express.static('/mnt/c/Videos'));
 
 mongoose.connect(config.getDbConnectionString(), {
     useUnifiedTopology: true
 });
+
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
-
     setupController(app);
     apiController(app);
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
-'use strict';
+app.listen(PORT, HOST, () => console.log('server running'));
 
 console.log('Hello world');
-

463c00e24891a638a45aa6c8f8b04e348d8ff80c
Toby Stone <40922682+stickmonster@users.noreply.github.com>
2024-03-08 10:20:40 +0000

    Set up for https

--- test/app.js
+++ test/app.js
@@ -1,29 +1,57 @@
 const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
 const config = require('./config');
-const setupController = require('./controllers/setupController');
+const { init } = require('./controllers/setupController');
 const apiController = require('./controllers/apiController');
+const https = require('https');
+const fs = require('fs');
 
+const app = express();
 const PORT = 3000;
 const HOST = '0.0.0.0';
 
 app.set('view engine', 'ejs');
-
 app.use('/public', express.static(__dirname + '/public'));
 
-mongoose.connect(config.getDbConnectionString(), {
-    useUnifiedTopology: true
-});
+// SSL options
+const options = {
+    key: fs.readFileSync('./ssl certificate/key.pem'),
+    cert: fs.readFileSync('./ssl certificate/cert.pem')
+};
 
-const db = mongoose.connection;
+// Initialize controllers and connect to DB
+(async () => {
+    await init(app);
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-    setupController(app);
     apiController(app);
-});
 
-app.listen(PORT, HOST, () => console.log('server running'));
+    // Start the server only if not in test mode
+    if (process.env.NODE_ENV !== 'test') {
+        // Create an HTTPS server and pass the Express app as the handler
+        const httpsServer = https.createServer(options, app);
+
+        httpsServer.listen(PORT, HOST, () => console.log(`Server running on ${HOST}:${PORT}`));
+    }
+})();
 
 console.log('Hello world');
+
+// Handle the SIGINT event to gracefully shut down the server
+process.on('SIGINT', function () {
+    console.log("\nGracefully shutting down from SIGINT (Ctrl+C)");
+    // Stop accepting new connections
+    server.close(async () => {
+        console.log("Closed out remaining connections.");
+        // Disconnect from the database
+        await mongoose.disconnect();
+        console.log("Disconnected from database.");
+        process.exit(0);
+    });
+
+    // If server hasn't finished in 10 seconds, shut down process forcibly
+    setTimeout(() => {
+        console.error("Could not close connections in time, forcefully shutting down");
+        process.exit(1);
+    }, 10000);
+});
+
+module.exports = app;
